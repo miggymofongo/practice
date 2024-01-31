802ce437b24e7278ae4f1546417e037f6df47b75
Miguel Almodovar <miguel@miguelalmodo.com>
2024-01-31 00:46:19 -0400

    learning about object literal syntax and using it to create objects on the fly!

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+/**  in this lesson I am using object literal syntax to 
+create objects on the fly. */
 
-var pet = {
-    name: "",
-    trimmedNails: false,
-    brushedFur: false,
-    trimNails: function() {
-        this.trimmedNails = true
-    },
-    brushFur: function() {
-        this.brushedFur = true
-    },
-    setName: function(callback) {
-      readline.question("What do you want to name your pet?", (name) => {
-        this.name = name;
-        console.log(`Pet's name set to: ${this.name}`);
-        readline.close();
-        if (callback) callback();
-      })
+var Miggy = { 
+    nombre: 'Miggy', 
+    apellido: 'Mofongo',
+    direccion: { 
+        calle: 'Car. 2',
+        ciudad: 'Aguadilla',
+        pais: 'Puerto Rico'
     }
+
+};
+
+
+
+function saludar(persona) {
+    console.log('Hola ' + persona.nombre);
 }
 
+function comida(persona){ 
+
+    console.log('Su comida favorita es ' + Miggy.comida);
+
+}
+/** these are two functions that will spit out two 
+ * greetings
+ */
+saludar(Miggy);
 
 
-let dog = Object.create(pet);
+/**this function is created on the fly using 
+ * literal sytax
+ */
+saludar({ 
+    nombre: 'Maria', 
+    apellido: 'Almodovar',
+})
 
-let cat = Object.create(pet);
-
-cat.meow = function() {
-
-    console.log('Meow');
+/**here i use a dot operator to add a new property
+ * then object literal syntax to define the property 
+ * as an object. 
+ */
+Miggy.comida = { 
+    comida: 'mofongo',
 }
 
-dog.trimNails();
-
-cat.setName();
-
-console.log(dog);
-
-cat.meow();
 
